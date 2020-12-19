e73be7a97dd168764ce246579070cd5e9513fea3
janani-ruwandharani <70389230+janani-ruwandharani@users.noreply.github.com>
2020-12-19 20:00:36 +0530

    downloads multiple videos

--- video_downloader.py
+++ video_downloader.py
@@ -7,25 +7,28 @@ from pytube import YouTube
 SAVE_PATH = "D:/movies"
 
 # link of the video to be downloaded
-link = 'https://www.youtube.com/watch?v=PYBIrAZ7KV4'
+link = {'https://www.youtube.com/watch?v=PYBIrAZ7KV4', 'https://www.youtube.com/watch?v=mdu5lLpMH_w',
+        'https://www.youtube.com/watch?v=tCjwDv5xGsM '}     # line break -to avoid line being too long -properly aligned
+# to check all the links use a for loop
+for i in link:
 
-try:
-    # object creation using YouTube
-    yt = YouTube(link)
+    try:
+        # object creation using YouTube
+        yt = YouTube(i)
 
-except:
-    print("Connection Failure")
+    except:
+        print("Connection Failure")
 
 
-# get the video with the extension and resolution
-d_video = yt.streams.get_highest_resolution()
+    # get the video with the extension and resolution
+    d_video = yt.streams.get_highest_resolution()
 
-try:
-    # download the video
-    d_video.download(SAVE_PATH)
+    try:
+        # download the video
+        d_video.download(SAVE_PATH)
 
-except:
-    print("An Error Occurred")
+    except:
+        print("An Error Occurred")
 
 print('Task Completed')
 
